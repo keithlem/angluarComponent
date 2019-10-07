9dc4e80e09b054dfb4dc276ff327f199ea9023c9
keithlem <keithlem9@gmail.com>
2019-10-06 21:55:28 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { ModalComponent } from './modal/modal.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MyComponentComponent, ModalComponent, CarouselComponent],
+  imports: [BrowserModule, NgbModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
